36ca335e6ee38b1f41d5b0f08dcda5c507c255b3
vsekar234 <vickysek234@gmail.com>
2026-01-28 12:17:19 +0530

    Addition for three numbers

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,8 @@
 # app.py
 # This is a test commits
-def add(a, b):
-    return a + b
+def add(a, b, c):
+    return a + b + c
 
 def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(1, 2, 4) == 3
+    assert add(1, -1, 5) == 0
